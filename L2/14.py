8564b845e1be73d768fcfece92e8f41d485c1986
Quetzalcoatlus2 <147990920+Quetzalcoatlus2@users.noreply.github.com>
2023-10-29 09:50:53 +0200

    Update 14.py

--- L2/14.py
+++ L2/14.py
@@ -1,11 +1,25 @@
 x=25
-bitNumber = 2
+bitNumber1 = 3
+bitNumber2 = 2
+
+#Se afiseaza valoarea bitului indicat
 rezultat = 1==(x>>bitNumber & 1)
 print(rezultat)
 
 print(bin(x))
 
-rezultat = x | (1<<bitNumber)
+#Transforma valoarea bitului in True
+rezultat = x | (1<<bitNumber1)
 print(rezultat)
-rezultat = x & ~(1<<bitNumber)
+
+#Transforma valoarea bitului in False
+rezultat = x & ~(1<<bitNumber1)
+print(rezultat)
+
+#Aceasta linie transforma starea adica din 0 in 1 si vice-versa
+rezultat = x ^ (1<<bitNumber1)
+print(rezultat)
+
+#Aceasta linie transforma simultan 2 biti
+rezultat = x ^ ((1<<bitNumber1)|1<<bitNumber2))
 print(rezultat)
