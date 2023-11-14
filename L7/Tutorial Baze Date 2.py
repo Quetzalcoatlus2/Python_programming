b3a8a1f00ed1d7ce2bec48f6bbb22b57f022df71
Quetzalcoatlus2 <bogdan-vlad.teodor@student.tuiasi.ro>
2023-11-14 08:03:34 +0200

    Refactored code to improve performance and readability

---
+++ L7/Tutorial Baze Date 2.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+
+#conn = None
+conn = sqlite3.connect("db1.db")
+      
+
+
+cursor=conn.cursor()
+cursor.execute("select * from users;")
+rezultat = cursor.fetchall()
+
+
+
+
+listaUseri = []
+for row in rezultat:
+   listaUseri.append(row[1])
+
+
+
+
+print(listaUseri)
+
+
+account = input("username:")
+
+
+#verific daca usernameul exista
+if account not in listaUseri:
+   print("error")
+else:
+   password = input("pass:")
+
+
+
+
+conn.close()
---
+++ L7/Tutorial Baze Date.py
@@ -0,0 +1,24 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+   """ create a database connection to a SQLite database """
+   conn = None
+   try:
+       conn = sqlite3.connect(db_file)
+       print(sqlite3.version)
+       cursor=conn.cursor()
+       cursor.execute("select * from users;")
+       print(__name__)
+       print(cursor.fetchall())
+
+   except Error as e:
+       print(e)
+   finally:
+       if conn:
+           conn.close()
+
+
+if __name__ == '__main__':
+   create_connection(r"C:\Users\teodo\Desktop\SQLite\db1.db")
+
