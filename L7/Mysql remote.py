f2063b305f57d23c79bb0ab0a8275ea976e7c517
Luisa <luisa-yi-qing.yang@student.tuiasi.ro>
2023-11-17 10:03:47 +0200

    remote

--- L7/Mysql remote.py
+++ L7/Mysql remote.py
@@ -3,26 +3,25 @@ from mysql.connector import Error
 
 try:
     connection = mysql.connector.connect(host='sql11.freemysqlhosting.net',
-                                         database='sql11405875',
-                                         user='sql11405875',
-                                         password='GcWhwdfA6a')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        query = '''CREATE TABLE noteTAD (
-        id  INTEGER NOT NULL PRIMARY KEY UNIQUE,
-        nume    TEXT,
-        prenume TEXT,
-        n1  INTEGER,
-        n2  INTEGER
-        );'''
-        cursor.execute(query)
-        print("Table created successfully.")
+                                         database='sql11662854',
+                                         user='sql11662854',
+                                         password='USx5upjQ3R')
+   if connection.is_connected():
+       db_Info = connection.get_server_info()
+       print("Connected to MySQL Server version ", db_Info)
+       cursor = connection.cursor()
+       #cursor.execute("select database();")
+       query = """SELECT * FROM python2023;"""
+       cursor.execute(query)
+       record = cursor.fetchall()
+       print(record)
+       for row in record:
+           print(row[1],(row[3] + row[4])/2)
+
+
 except Error as e:
-    print("Error while connecting to MySQL", e)
+   print("Error while connecting to MySQL", e)
 finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed") 
+   if connection.is_connected():
+       cursor.close()
+       connection.close()
