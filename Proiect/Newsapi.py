a1b4ba6a40bbc4184e94fde4e3fbe616fa8230f6
Quetzalcoatlus2 <bogdan-vlad.teodor@student.tuiasi.ro>
2023-12-27 17:26:54 +0200

    Fix error handling and add additional news sources

--- Proiect/Newsapi.py
+++ Proiect/Newsapi.py
@@ -22,15 +22,15 @@ def get_articles(apiKey, language, country, category, pageSize, q, sources):
         response = requests.get(newsapi_url, parameters)
         response.raise_for_status()  # Raises a HTTPError if the status is 4xx, 5xx
         return response.json()['articles']
-    except RequestException as e:
-        print(f"Network error: {e}")
-        return None
+    except RequestException as error:
+        print(f"Network error: {error}")
+        return None, error
 
     if response.status_code == 200:
         return response.json()['articles']
     else:
         print(f"ERROR {response.status_code}: {response.text}")
-        return None
+        return None, response.status_code, response.text
     
 
 def display_articles_gui(articles):
@@ -52,7 +52,14 @@ def display_articles_gui(articles):
     else:
         message = ttk.Label(window, text="No articles found. Try again.", font=("Arial", 12))
         message.grid(row=0, column=0, sticky=(tk.W))
-
+    if e:
+        error = ttk.Label(window, text=f"Network error: {error}", font=("Arial", 12))
+        error.grid(row=1, column=0, sticky=(tk.W))
+    if response.status_code != 200:
+        response_error = ttk.Label(window, text=f"ERROR {response.status_code}: {response.text}", font=("Arial", 12))
+        response_error.grid(row=1, column=0, sticky=(tk.W))
+        print(f"ERROR {response.status_code}: {response.text}")
+        return None
     window.mainloop()
 
 #def display_articles(articles):
@@ -75,8 +82,8 @@ if __name__ == "__main__":
 
     # For 'category', fill in: general, business, entertainment, health, science, sports, technology
 
-    # For 'sources', fill in sources={'google-news', 'bbc-news', 'the-verge'}
+    # For 'sources', fill in sources={'google-news', 'bbc-news', 'the-verge', 'cnn', 'fox-news', 'the-washington-post', 'the-new-york-times', 'the-wall-street-journal'}
     # ATTENTION!!! The "sources" field cannot be mixed with the "country" and "category" fields, so either use sources, or 
     # use "country" + "category"
-    articles = get_articles(apiKey, language=None, country=None, category=None, sources=None, pageSize=20, q='coronavirus')
+    articles, error, response.status_code, response.text = get_articles(apiKey, language=None, country=None, category=None, sources={'cnn', 'google-news'}, pageSize=20, q='biden')
     display_articles_gui(articles)
