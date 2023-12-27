4d8d392373de8c401d61a053dc554af2b7f39e90
Quetzalcoatlus2 <bogdan-vlad.teodor@student.tuiasi.ro>
2023-12-27 19:28:31 +0200

    Refactor display_articles_gui function to use a GUI interface

--- Proiect/Newsapi.py
+++ Proiect/Newsapi.py
@@ -64,35 +64,57 @@ def display_articles_gui(articles, error, status_code, text):
 
     window.mainloop()
 
-#def display_articles(articles):
-#    if articles:
-#        for i, article in enumerate(articles, start = 1):
-#            print(f"#{i} {article['title']}")
-#            print(f"   Article source: {article['source']['name']}")
-#            print(f"   Link: {article['url']}")
-#            print("\n")
-#    else:
-#        print("No articles found. Try again.")
+
+def gui(articles):
+    window = tk.Tk()
+    window.title("News Articles")
+    if articles:
+        for i, article in enumerate(articles, start=1):
+             frame = ttk.Frame(window, padding="10")
+             frame.grid(row=i, sticky=(tk.W, tk.E))
+
+             title = ttk.Label(frame, text=f"#{i} {article['title']}", font=("Verdana", 10))
+             title.grid(row=0, column=0, sticky=(tk.W))
+
+             source = ttk.Label(frame, text=f"Source: {article['source']['name']}", font=("Arial", 12))
+             source.grid(row=1, column=0, sticky=(tk.W))
+
+             url = ttk.Label(frame, text=f"URL: {article['url']}", font=("Arial", 12))
+             url.grid(row=2, column=0, sticky=(tk.W))
+    
+"""def display_articles(articles):
+    if articles:
+        for i, article in enumerate(articles, start = 1):
+            print(f"#{i} {article['title']}")
+            print(f"   Article source: {article['source']['name']}")
+            print(f"   Link: {article['url']}")
+            print("\n")
+    else:
+        print("No articles found. Try again.")"""
 
 if __name__ == "__main__":
-    # API key source: https://newsapi.org/
     apiKey = '33064a07856d4cf98dd5fd5d759d3ef4'
-    #error 401 if API key is invalid or missing
-    
-    # For 'country', fill in the country code in uppercase, lowercase, or both (NewsAPI supports 54 countries):
-    # ae ar at AU be bg br CA ch cn co cu cz de eg FR GB gr HK hu id ie il IN it jp kr 
-    # lt lv ma mx my ng nl no NZ ph pl pt RO rs ru sa se sg si sk th tr tw ua US ve za
-
-    # For 'category', fill in: general, business, entertainment, health, science, sports, technology
-
-    # For 'sources', fill in sources={'google-news', 'bbc-news', 'the-verge', 'cnn', 'usa-today', 'abc-news', 
-    # 'associated-press', 'axios', 'bloomberg', 'business-insider', 'cbc-news', 'cbs-news', 'cnbc', 'engadget', 
-    # 'entertainment-weekly', 'fortune', 'fox-sports', 'google-news-ca', 'google-news-uk', 'hacker-news', 'ign', 
-    # 'medical-news-today', 'msnbc', 'mtv-news', 'national-geographic', 'nbc-news', 'news24', 'newsweek', 
-    # 'new-york-magazine', 'next-big-future', 'nfl-news', 'nhl-news', 'politico', 'polygon', 'recode', 'reddit-r-all', 
-    # 'reuters', 'techcrunch', 'techradar', 'the-american-conservative', 'the-hill', 'the-huffington-post', 
-    # 'the-next-web', 'the-sport-bible', 'the-times-of-india', 'the-washington-times', 'time', 'usa-today', 'vice-news', 'wired'}
-    #
-    # ATTENTION!!! The "sources" field cannot be mixed with the "country" and "category" fields, so either use sources, or use "country" + "category"
-    articles, error, status_code, text = get_articles(apiKey, language='en', country=None, category=None, sources=None , pageSize=5, q='sex')
+    articles, error, status_code, text = get_articles(apiKey, language='en', country=None, category=None, sources=None , pageSize=5, q='sex')    
     display_articles_gui(articles, error, status_code, text)
+
+
+"""
+API key source: https://newsapi.org/
+error 401 if API key is invalid or missing
+
+For 'country', fill in the country code in uppercase, lowercase, or both (NewsAPI supports 54 countries):
+ae ar at AU be bg br CA ch cn co cu cz de eg FR GB gr HK hu id ie il IN it jp kr 
+lt lv ma mx my ng nl no NZ ph pl pt RO rs ru sa se sg si sk th tr tw ua US ve za
+
+For 'category', fill in: general, business, entertainment, health, science, sports, technology
+
+For 'sources', fill in sources={'google-news', 'bbc-news', 'the-verge', 'cnn', 'usa-today', 'abc-news', 
+'associated-press', 'axios', 'bloomberg', 'business-insider', 'cbc-news', 'cbs-news', 'cnbc', 'engadget', 
+'entertainment-weekly', 'fortune', 'fox-sports', 'google-news-ca', 'google-news-uk', 'hacker-news', 'ign', 
+'medical-news-today', 'msnbc', 'mtv-news', 'national-geographic', 'nbc-news', 'news24', 'newsweek', 
+'new-york-magazine', 'next-big-future', 'nfl-news', 'nhl-news', 'politico', 'polygon', 'recode', 'reddit-r-all', 
+'reuters', 'techcrunch', 'techradar', 'the-american-conservative', 'the-hill', 'the-huffington-post', 
+'the-next-web', 'the-sport-bible', 'the-times-of-india', 'the-washington-times', 'time', 'usa-today', 'vice-news', 'wired'}
+    
+ ATTENTION!!! The "sources" field cannot be mixed with the "country" and "category" fields, so either use sources, or use "country" + "category
+ """
