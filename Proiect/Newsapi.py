078d137ea32e759262cf452eeb37591f69abb1bc
Quetzalcoatlus2 <bogdan-vlad.teodor@student.tuiasi.ro>
2023-12-30 12:22:13 +0200

    Refactor GUI code and fix scrolling issue

--- Proiect/Newsapi.py
+++ Proiect/Newsapi.py
@@ -2,7 +2,6 @@ import requests
 import tkinter as tk
 from requests.exceptions import RequestException
 from newsapi import NewsApiClient
-from tkinter import ttk, Label, Button, Entry, Frame, Text, Scrollbar, Canvas, W, E, N, S
 from PIL import Image, ImageTk
 from io import BytesIO
 import webbrowser
@@ -11,18 +10,23 @@ newsapi = NewsApiClient(api_key='33064a07856d4cf98dd5fd5d759d3ef4')
 
 previous_window = None
 
-def open_url(event, url):
+def open_url(action, url):
     webbrowser.open(url)
 
+
+def on_mousewheel(action):
+    canvas.yview_scroll(int(-1*(action.delta/80)), "u")
+
+
 def buttons(i):
-    L1 = Label(window, text = 'Cuvânt cheie:')
-    L1.grid(row = i, sticky = W)
-    E1 = Entry(window, bd=5)
-    E1.grid(row = i, padx = 80, sticky = W)
-    button = Button(window, text = "Căutare", command = lambda: keyword_articles(E1.get()))
-    button.grid(row = i, padx = 220, sticky = W)
-    L2 = Label(window, text = f"Număr rezultate:{totalResults}")
-    L2.grid(row=i+1, sticky=(W)) 
+    L1 = tk.Label(window, text = 'Cuvânt cheie:')
+    L1.grid(row = i, sticky = tk.W)
+    E1 = tk.Entry(window, bd=5)
+    E1.grid(row = i, padx = 80, sticky = tk.W)
+    button = tk.Button(window, text = "Căutare", command = lambda: keyword_articles(E1.get()))
+    button.grid(row = i, padx = 220, sticky = tk.W)
+    L2 = tk.Label(window, text = f"Număr rezultate:{totalResults}")
+    L2.grid(row=i+1, sticky=(tk.W)) 
 
 def get_articles(apiKey, language, country, category, pageSize, q, sources):
     newsapi_url = 'https://newsapi.org/v2/top-headlines'
@@ -69,13 +73,16 @@ def keyword_articles(E1):
     articles, totalResults, error, status, code, message = get_articles(apiKey, language='en', country=None, category=None, sources=None , pageSize=2, q=f'{E1}')    
     display_articles_gui(articles, error, status, code, message)
 
-def exit_fullscreen(event):
+def exit_fullscreen():
     window.attributes('-fullscreen', False)
+
+
 
 def display_articles_gui(articles, error, status, code, message):
 
     global previous_window
     global window
+    global canvas
 
     if previous_window is not None:
         previous_window.destroy()
@@ -85,73 +92,74 @@ def display_articles_gui(articles, error, status, code, message):
     #window.attributes('-fullscreen', True)
     #window.bind('<Escape>', exit_fullscreen)
 
-    canvas = Canvas(window)
-    scrollbar = Scrollbar(window, orient='vertical', command=canvas.yview)
+    canvas = tk.Canvas(window)
+    scrollbar = tk.Scrollbar(window, orient='vertical', command=canvas.yview)
     canvas.configure(yscrollcommand=scrollbar.set) 
-    frame = Frame(canvas)
-    canvas.pack(side='left', fill='both', expand=True)
-    scrollbar.pack(side='right', fill='y')
-    # canvas.grid(row=0, column=0, sticky=(N, S, E, W))
-    # scrollbar.grid(row=0, column=1, sticky=(N, S))
+    frame = tk.Frame(canvas)
+    canvas.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+    scrollbar.grid(row=0, column=1, sticky=(tk.N, tk.S))
     canvas.create_window((0, 0), window=frame, anchor='nw')
+    canvas.bind_all("<MouseWheel>", on_mousewheel)
+    window.grid_rowconfigure(0, weight=1)
+    window.grid_columnconfigure(0, weight=1)
 
     if articles:
         for i, article in enumerate(articles, start=1):
-            article_frame = Frame(frame, padx=10)
-            article_frame.grid(row=i, sticky=(W, E))
+            article_frame = tk.Frame(frame, padx=10)
+            article_frame.grid(row=i, sticky=(tk.W, tk.E))
 
-            title_label = Label(article_frame, text=f"#{i} {article['title']}", font=("Verdana", 10))
-            title_label.grid(row=0, sticky=(W))
+            title_label = tk.Label(article_frame, text=f"#{i} {article['title']}", font=("Verdana", 10))
+            title_label.grid(row=0, sticky=(tk.W))
 
-            source_label = Label(article_frame, text=f"Sursă: {article['source']['name']}", font=("Arial", 12))
-            source_label.grid(row=1, sticky=(W))
+            source_label = tk.Label(article_frame, text=f"Sursă: {article['source']['name']}", font=("Arial", 12))
+            source_label.grid(row=1, sticky=(tk.W))
 
-            author_label = Label(article_frame, text=f"Autori: {article['author']}", font=("Arial", 12))
-            author_label.grid(row=2, sticky=(W))
+            author_label = tk.Label(article_frame, text=f"Autori: {article['author']}", font=("Arial", 12))
+            author_label.grid(row=2, sticky=(tk.W))
 
-            description_label = Label(article_frame, text=f"Scurtă descriere: {article['description']}", font=("Arial", 12))
-            description_label.grid(row=3, sticky=(W))
+            description_label = tk.Label(article_frame, text=f"Scurtă descriere: {article['description']}", font=("Arial", 12))
+            description_label.grid(row=3, sticky=(tk.W))
 
-            publishedAt_label = Label(article_frame, text=f"Publicat la: {article['publishedAt']}", font=("Arial", 12))
-            publishedAt_label.grid(row=7, sticky=(W))
+            publishedAt_label = tk.Label(article_frame, text=f"Publicat la: {article['publishedAt']}", font=("Arial", 12))
+            publishedAt_label.grid(row=7, sticky=(tk.W))
 
-            url_label = Label(article_frame, text=f"Link: {article['url']}", font=("Arial", 12), fg="blue", cursor="hand2")
-            url_label.grid(row=4, sticky=(W))
+            url_label = tk.Label(article_frame, text=f"Link: {article['url']}", font=("Arial", 12), fg="blue", cursor="hand2")
+            url_label.grid(row=4, sticky=(tk.W))
             url_label.bind("<Button-1>", lambda event, url=article['url']: open_url(event, url))
 
-            urlToImage_label = Label(article_frame, text=f"Link imagine: {article['urlToImage']}", font=("Arial", 12), fg="blue", cursor="hand2")
-            urlToImage_label.grid(row=5, sticky=(W))
+            urlToImage_label = tk.Label(article_frame, text=f"Link imagine: {article['urlToImage']}", font=("Arial", 12), fg="blue", cursor="hand2")
+            urlToImage_label.grid(row=5, sticky=(tk.W))
             urlToImage_label.bind("<Button-1>", lambda event, url=article['urlToImage']: open_url(event, url))
 
             
-            response = requests.get(article['urlToImage'])
-            img_data = response.content
+            response_urlToImage = requests.get(article['urlToImage'])
+            img_data = response_urlToImage.content
             img = Image.open(BytesIO(img_data))
             photo = ImageTk.PhotoImage(img)
 
             
-            img_label = Label(article_frame, image=photo)
+            img_label = tk.Label(article_frame, image=photo)
             img_label.image = photo 
-            img_label.grid(row=6, sticky=(W))
+            img_label.grid(row=5, sticky=(tk.W))
 
            
 
         buttons(i+1)
 
     elif error:
-        error = Label(window, text=f"Network error: {error}", font=("Arial", 12))
-        error.grid(row=1, column=0, sticky=(W))
-        status = Label(window, text=f"Status: {status}", font=("Arial", 12))
-        status.grid(row=2, column=0, sticky=(W))
-        code = Label(window, text=f"Cod: {code}", font=("Arial", 12))
-        code.grid(row=3, column=0, sticky=(W))
-        message = Label(window, text=f"Mesaj: {message}", font=("Arial", 12))
-        message.grid(row=4, column=0, sticky=(W))
+        error = tk.Label(window, text=f"Network error: {error}", font=("Arial", 12))
+        error.grid(row=1, column=0, sticky=(tk.W))
+        status = tk.Label(window, text=f"Status: {status}", font=("Arial", 12))
+        status.grid(row=2, column=0, sticky=(tk.W))
+        code = tk.Label(window, text=f"Cod: {code}", font=("Arial", 12))
+        code.grid(row=3, column=0, sticky=(tk.W))
+        message = tk.Label(window, text=f"Mesaj: {message}", font=("Arial", 12))
+        message.grid(row=4, column=0, sticky=(tk.W))
         buttons(5)
 
     else:
-        no_articles = Label(window, text="Nu s-au găsit articole. Încercați din nou.", font=("Arial", 12))
-        no_articles.grid(row=0, column=0, sticky=(W))
+        no_articles = tk.Label(window, text="Nu s-au găsit articole. Încercați din nou.", font=("Arial", 12))
+        no_articles.grid(row=0, column=0, sticky=(tk.W))
         buttons(1)
     
     window.update()
