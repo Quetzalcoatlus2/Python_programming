540ebff91e136bf467bbfc83ef10bd61c36afde0
Quetzalcoatlus2 <bogdan-vlad.teodor@student.tuiasi.ro>
2024-01-19 07:39:23 +0200

    Increase occurrences of "Like" in test string

--- L4/Piramidă.py
+++ L4/Piramidă.py
@@ -5,5 +5,6 @@ strat = 1
 while nr >= strat:
     nr -= strat
     strat += 1
-else:
-    print("Piramida are", strat-1, "nivele.\nNu au fost folosite", nr, "blocuri.")
+
+print("Piramida are", strat-1, "nivele.")
+print("Nu au fost folosite", nr, "blocuri.")
